eb54ffa6c7a30c6ff155acdd092e525b9fcefdd5
DevClass <support-2@ahsmail.com>
2018-07-16 10:47:13 -0600

    figured that part out

--- src/components/home.js
+++ src/components/home.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import Card from './card';
 import Header from './header';
-import Input from './input';
-class Home extends Component {
+
+export default class Home extends Component {
   render() {
     return (
-      <div>
+      <div className="home">
         <h1>DevCamp React Starter</h1>
         { Header() }
         <Card />
